0b0e2671498c46245a91a397046b5719e1adb289
Prosper Banda <prosperbanda@meta.com>
2023-06-21 15:20:17 -0700

    Added the search and filter section

--- student-store-ui/src/components/SearchAndFilter/SearchAndFilter.jsx
+++ student-store-ui/src/components/SearchAndFilter/SearchAndFilter.jsx
@@ -4,7 +4,7 @@ export function SearchAndFilter(props) {
     return (
       <div className="searchAndFilter">
         <div className="content">
-            <div className="row">
+            <div className="row1">
                 <div className="searchBar">
                     <input type="text" name="search" placeholder="Search"/>
                     <i className="material-icons">search</i>
@@ -12,9 +12,35 @@ export function SearchAndFilter(props) {
                 <div className="links">
                     <span className="help">
                         <i className="material-icons">help</i>
-                        "Help"
+                        Help
                     </span>
+                    <div className="cart">
+                        My Cart
+                        <i className="material-icons">shopping_cart</i>
+                    </div>
                 </div>
+            </div>
+            <div className="row2">
+                <div className="hamburger-menu">
+                    <i className="material-icons">menu</i>
+                </div>
+                <ul className="category-menu">
+                    <li className="is-active">
+                        <button>All Categories</button>
+                    </li>
+                    <li>
+                        <button>Clothing</button>
+                    </li>
+                    <li>
+                        <button>Food</button>
+                    </li>
+                    <li>
+                        <button>Accessories</button>
+                    </li>
+                    <li>
+                        <button>Tech</button>
+                    </li>
+                </ul>
             </div>
         </div>
       </div>
