d232b67f2e5b7bad879154bc0daf0b38d21872a7
Prosper Banda <prosperbanda@meta.com>
2023-06-22 02:23:02 -0700

    Fetched and displayed the products data on the home page in a grid view

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,24 +1,62 @@
 import * as React from "react"
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from "../Navbar/Navbar"
-import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import "./App.css"
 import Hero from "../Hero/Hero"
 import SearchAndFilter from "../SearchAndFilter/SearchAndFilter"
+import { useState } from "react"
+import { useEffect } from "react"
+import axios from 'axios'
 
 export default function App() {
+  
   return (
     <div className="app">
       <BrowserRouter>
         <main>
           <Navbar />
-          <Sidebar />
           <Hero/>
           <SearchAndFilter/>
+          <HomePage/>
           <Home />
         </main>
       </BrowserRouter>
     </div>
   )
 }
+
+const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://codepath-store-api.herokuapp.com/store'); 
+      setProducts(response.data.products);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div className="grid-container">
+      {products.map((product) => (
+        <div className="product-card" key={product.id}>
+          <img src={product.image} alt={product.name} />
+          <h3>{product.name}</h3>
+          <p>Price: ${product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+
+
+
--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,12 @@
-import * as React from "react"
-import "./Sidebar.css"
+// import * as React from "react"
+// import "./Sidebar.css"
 
-export default function Sidebar() {
-  return (
-    <section className="sidebar">
-        <div className="wrapper">
-          <h3>Sidebar</h3>
-        </div>
-    </section>
-  )
-}
+// export default function Sidebar() {
+//   return (
+//     <section className="sidebar">
+//         <div className="wrapper">
+//           <h3>Sidebar</h3>
+//         </div>
+//     </section>
+//   )
+// }
